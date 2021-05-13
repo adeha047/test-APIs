7d9846a694558fcfb8e671502eefe271d6888596
adeha047 <dehaven7777@gmail.com>
2021-05-13 10:06:43 -0400

    added new files

---
+++ test/src/utils/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const params = {
+  access_key: 'f8eb8443250915180bc05d7229510863'
+}
+
+axios.get('https://api.aviationstack.com/v1/flights', {params})
+  .then(response => {
+    const apiResponse = response.data;
+    if (Array.isArray(apiResponse['results'])) {
+        apiResponse['results'].forEach(flight => {
+            if (!flight['live']['is_ground']) {
+                console.log(`${flight['airline']['name']} flight ${flight['flight']['iata']}`,
+                    `from ${flight['departure']['airport']} (${flight['departure']['iata']})`,
+                    `to ${flight['arrival']['airport']} (${flight['arrival']['iata']}) is in the air.`);
+            }
+        });
+    }
+  }).catch(error => {
+    console.log(error);
+  });
